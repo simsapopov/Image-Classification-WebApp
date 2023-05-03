7a52d72da7182258f0269629ddd48822b5b7c635
Simeon Popov <simsapopov@abv.bg>
2023-05-03 12:39:48 +0300

    Add Greeting GreetingController and UserService

---
+++ src/main/java/com/example/ics/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.ics;
+
+public record Greeting(long id, String content) { }
---
+++ src/main/java/com/example/ics/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.ics.service;
+import org.springframework.stereotype.Service;
+@Service
+public class UserService {
+    public String helloWorld(){
+        return "Hello, world!";
+    }
+}
