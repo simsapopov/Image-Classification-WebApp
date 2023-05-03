e4a06a9ddd80691b28b215c841b40ee01ac5326b
Simeon Popov <simsapopov@abv.bg>
2023-05-03 12:34:58 +0300

    Initial commit

---
+++ src/test/java/com/example/ics/IcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
