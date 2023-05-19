a6bdefdb7aa30c4dcff8ed3ded67db1cd6a077ad
Simeon Popov <simsapopov@abv.bg>
2023-05-19 14:54:02 +0300

    TagService

--- src/main/java/com/example/ics/Reposittory/TagRepository.java
+++ src/main/java/com/example/ics/Reposittory/TagRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     List<Tag> findByimage_id(Long imageId);
+
 }
--- src/main/java/com/example/ics/Service/TagService.java
+++ src/main/java/com/example/ics/Service/TagService.java
@@ -1,4 +1,29 @@
 package com.example.ics.Service;
+import com.example.ics.Entity.Images;
+import com.example.ics.Entity.Tag;
+import com.example.ics.Reposittory.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TagService {
+    private final TagRepository tagRepository;
+
+
+    public List<Tag> getAllTagsWithID(Long ID) {
+        return tagRepository.findByimage_id(ID);
+    }
+    public void addTag(long ID, String tag,double confidence,Images image){
+        Tag newTag = new Tag();
+        newTag.setId(ID);
+        newTag.setName(tag);
+        newTag.setConfidencePercentage(confidence);
+        newTag.setImage(image);
+        tagRepository.save(newTag);
+    }
+
+
 }
